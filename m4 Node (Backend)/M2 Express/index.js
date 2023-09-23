91e83f502119517ffe9c5db329ad35fa9f17b3ae
Pulkit Shringi <pulkitshringi@yahoo.com>
2023-09-23 21:36:18 +0530

    updated

--- m4 Node (Backend)/M2 Express/index.js
+++ m4 Node (Backend)/M2 Express/index.js
@@ -4,3 +4,23 @@ let port = 3000;
 app.listen(port , ()=>{
     console.log(`You are listening from port ${port}`);
 })
+// app.use((req,res)=>{
+//     console.log("Response generated");
+//     res.send({
+//         name:"Pulkit",
+//         Age:21
+//     });
+// })
+
+app.post("/" , (req,res)=>{
+    res.send("You contacted rooth path");
+});
+app.post("/search" , (req,res)=>{
+    res.send("You contacted search path");
+});
+app.post("/navigation" , (req,res)=>{
+    res.send("You contacted navigation path");
+});
+app.post("*",(req,res)=>{
+    res.send("This page does not exist");
+})
