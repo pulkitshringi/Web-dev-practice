e9c0d8e7b617c99e13d98b6845f01a7503949077
Pulkit Shringi <pulkitshringi@yahoo.com>
2023-10-01 00:45:30 +0530

    up

--- m4 Node (Backend)/M3 EJSdir/index.js
+++ m4 Node (Backend)/M3 EJSdir/index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
+const instaDatabase = require("./data.json");
+const e = require("express");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"public/css")));
+app.use(express.static(path.join(__dirname,"public/js")));
 app.listen(port,()=>{
     console.log("Server is on");
 });
-app.get("/",(req,res)=>{
-    res.render("home.ejs");
+app.get("/ig/:username",(req,res)=>{
+   let {username} = req.params;
+   if(instaDatabase[username]){
+   res.render("instagram.ejs",{data:instaDatabase[username]});
+   }
+   else
+   res.render("error.ejs",{username});
 })
---
+++ m4 Node (Backend)/M3 EJSdir/public/js/app.js
@@ -0,0 +1,6 @@
+let btns = document.querySelectorAll("button");
+for(btn of btns){
+    btn.addEventListener("click",()=>{
+        alert("Button was Clicked");
+    })
+}
