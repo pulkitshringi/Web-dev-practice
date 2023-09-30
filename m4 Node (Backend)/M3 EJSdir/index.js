c386a26e458c81156761bd26b983d7152483346b
Pulkit Shringi <pulkitshringi@yahoo.com>
2023-09-30 21:38:08 +0530

    updated

--- m4 Node (Backend)/M3 EJSdir/index.js
+++ m4 Node (Backend)/M3 EJSdir/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 app.listen(port,()=>{
     console.log("Server is on");
 });
 app.get("/",(req,res)=>{
-    res.send("homepage");
+    res.render("home.ejs");
 })
