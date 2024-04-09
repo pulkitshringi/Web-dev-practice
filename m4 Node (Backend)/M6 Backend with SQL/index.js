0d77dd9ba7866b4f2379bff470e5ade3d27e336f
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-04-09 22:57:49 +0530

    updated

--- m4 Node (Backend)/M6 Backend with SQL/index.js
+++ m4 Node (Backend)/M6 Backend with SQL/index.js
@@ -1,10 +1,31 @@
 const { faker } = require('@faker-js/faker');
 let getRandomUser = ()=>{
-    return {
-        userId: faker.string.uuid(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      };
+    return [
+         faker.string.uuid(),
+         faker.internet.userName(),
+         faker.internet.email(),
+        faker.internet.password()
+    ];
     }
-    console.log(getRandomUser());
+    const mysql = require('mysql2');
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'delta_app',
+      password: 'admin@123'
+    });
+    let q = "INSERT INTO user(id,username,email,password) VALUES ?";
+    let users = [];
+    for(let i =0;i<100;i++){
+      users.push(getRandomUser());
+    }
+    try{
+    connection.query(q,[users],(err,result)=>{
+    if(err) throw err;
+    console.log(result);                                                     
+    })
+    }
+    catch(err){
+    console.log(err)
+    } 
+    connection.end();
