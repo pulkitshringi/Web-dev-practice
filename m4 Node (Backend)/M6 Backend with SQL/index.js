88d6bc6b77c774efef1ebb9609bffdb29263a175
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-04-18 08:56:12 +0530

    updated

--- m4 Node (Backend)/M6 Backend with SQL/index.js
+++ m4 Node (Backend)/M6 Backend with SQL/index.js
@@ -1,31 +1,42 @@
-const { faker } = require('@faker-js/faker');
-let getRandomUser = ()=>{
-    return [
-         faker.string.uuid(),
-         faker.internet.userName(),
-         faker.internet.email(),
-        faker.internet.password()
-    ];
-    }
-    const mysql = require('mysql2');
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const path = require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
     const connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
       database: 'delta_app',
       password: 'admin@123'
     });
-    let q = "INSERT INTO user(id,username,email,password) VALUES ?";
-    let users = [];
-    for(let i =0;i<100;i++){
-      users.push(getRandomUser());
-    }
-    try{
-    connection.query(q,[users],(err,result)=>{
-    if(err) throw err;
-    console.log(result);                                                     
+ 
+    app.listen(8080,()=>{
+      console.log("Server is listening to 8080");
+    });
+    // app.get("/",(req,res)=>{
+    //   let q = "SELECT COUNT(*) FROM user";
+    //   try{
+    //     connection.query(q,(err,result)=>{
+    //       if(err) throw err;
+    //       res.send(result);
+    //     })
+    //   }
+    //   catch(err){
+    //     console.log(err);
+    //   }
+    // })
+    app.get("/",(req,res)=>{
+      let q = "SELECT COUNT(*) FROM user";
+      try{
+      connection.query(q,(err,result)=>{
+        if(err) throw err;
+        let count = result[0]["COUNT(*)"];
+        res.render("home.ejs",{count});
+      })
+      }
+      catch(err){
+        console.log("error")
+        res.send("There's a error");
+      }
     })
-    }
-    catch(err){
-    console.log(err)
-    } 
-    connection.end();
