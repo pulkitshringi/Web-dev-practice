8b1150da0756c78e390c236a8716429258605b8e
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-09-17 02:26:45 +0530

    updated

--- m9Nextjs Maximilian /02-reactReferesher/Twitter Project/src/main.jsx
+++ m9Nextjs Maximilian /02-reactReferesher/Twitter Project/src/main.jsx
@@ -2,10 +2,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {RouterProvider,createBrowserRouter} from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([{path:'/'}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>
 )
