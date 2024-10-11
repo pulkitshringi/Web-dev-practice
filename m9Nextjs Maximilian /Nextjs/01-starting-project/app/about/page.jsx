2494b04a40dea31554067a9399b3738bad10c762
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-10-11 09:41:54 +0530

    updated

---
+++ m9Nextjs Maximilian /Nextjs/01-starting-project/app/about/page.jsx
@@ -0,0 +1,9 @@
+// app/about/page.jsx
+export default function About(){
+    return(
+        <main>
+            <h1>About Page</h1>
+            <p>This is the about page</p>
+        </main>
+    )
+}
