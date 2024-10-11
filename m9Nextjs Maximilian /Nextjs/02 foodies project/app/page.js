c2e1fb469a11021348f17cbb3f7d6a3427a6bb7a
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-10-11 12:14:38 +0530

    updated

---
+++ m9Nextjs Maximilian /Nextjs/02 foodies project/app/page.js
@@ -0,0 +1,10 @@
+// app/page.js
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+    </main>
+  );
+}
